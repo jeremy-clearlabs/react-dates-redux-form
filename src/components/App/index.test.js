97132993b583a437410833cf3938b55816c84025
jermspeaks <jermspeaks@gmail.com>
2018-05-24 12:48:42 -0700

    Add tests

--- src/components/App/index.test.js
+++ src/components/App/index.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Provider } from 'react-redux'
+import { StaticRouter } from 'react-router-dom'
+import App from '.';
+import configureStore from 'redux-mock-store' //ES6 modules
+
+const middlewares = []
+const mockStore = configureStore(middlewares);
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    (
+      <Provider store={mockStore({})}>
+        <StaticRouter context={{}}>
+          <App />
+        </StaticRouter>
+      </Provider>
+    )
+  , div);
   ReactDOM.unmountComponentAtNode(div);
 });
